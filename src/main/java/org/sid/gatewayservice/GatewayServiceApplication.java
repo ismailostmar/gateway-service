5fb63595370cd17462fbac1fd6b26583b1c6c0b8
DEVGEEKTM <ismailostmar@gmail.com>
2022-02-19 20:16:45 +0100

    .....//

--- src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -15,15 +13,6 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
-
-////	@Bean
-//	RouteLocator routeLocator(RouteLocatorBuilder builder){
-//		return builder.routes()
-//				.route((r)-> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
-//				.route((r)-> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
-//				.build();
-//}
 
 	// Dynamic Configuration like : http://localhost:8083/CUSTOMER-SERVICE/customers it will bring me infos about customers services dynamically
 	@Bean
